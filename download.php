c22e2c7c5d094b1f2e14291f60fab10336e3b0ff
NamitaAshodia <47408120+NamitaAshodia@users.noreply.github.com>
2021-05-10 11:44:59 +0530

    Add files via upload

---
+++ download.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Jagdish tools</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- Favicons -->
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  <!-- =======================================================
+  * Template Name: Baker - v2.1.0
+  * Template URL: https://bootstrapmade.com/baker-free-onepage-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+  <style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+/* Float four columns side by side */
+.column {
+  float: left;
+  width: 25%;
+  padding: 0 10px;
+}
+
+/* Remove extra left and right margins, due to padding */
+.row {margin: 0 -5px;}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive columns */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+    display: block;
+    margin-bottom: 20px;
+  }
+}
+
+/* Style the counter cards */
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 16px;
+  text-align: center;
+  background-color: #f1f1f1;
+}
+.btn-login{
+
+	padding: 15px 25px;
+	border: none;
+	background-color: #27ae60;
+	color: #fff;
+}
+</style>
+</head>
+
+<body>
+
+ <!--<header id="header" class="fixed-top header-transparent">-->
+ <header id="header" class="fixed-top d-flex align-items-center justify-content-center ">
+  
+  <!-- <section id="hero" class="d-flex align-items-center justify-content-center">-->
+     <div class="container position-relative">
+   
+     <div class="container d-flex align-items-center">
+ 
+       <h1 class="logo mr-auto"><a href="index.php">Jagdish tools</a></h1>
+       <!-- Uncomment below if you prefer to use an image logo -->
+       <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+ 
+       <nav class="nav-menu d-none d-lg-block">
+         <ul>
+           <li class="active"><a href="index">Home</a></li>
+           <li><a href="index#about">About us</a></li>
+           <li><a href="index#services">Products</a></li>
+        <!--   <li><a href="#portfolio">Portfolio</a></li>
+           <li><a href="#team">Team</a></li>
+ -->
+           <li><a href="download">Download</a></li>
+         <!--  <li class="drop-down"><a href="">Drop Down</a>
+             <ul>
+               <li><a href="#">Drop Down 1</a></li>
+               <li class="drop-down"><a href="#">Deep Drop Down</a>
+                 <ul>
+                   <li><a href="#">Deep Drop Down 1</a></li>
+                   <li><a href="#">Deep Drop Down 2</a></li>
+                   <li><a href="#">Deep Drop Down 3</a></li>
+                   <li><a href="#">Deep Drop Down 4</a></li>
+                   <li><a href="#">Deep Drop Down 5</a></li>
+                 </ul>
+               </li>
+               <li><a href="#">Drop Down 2</a></li>
+               <li><a href="#">Drop Down 3</a></li>
+               <li><a href="#">Drop Down 4</a></li>
+             </ul>
+           </li>-->
+           <li><a href="index#contact">Contact</a></li>
+ 
+         </ul>
+       </nav><!-- .nav-menu -->
+ 
+     </div>
+     </div>
+  <!-- </section>-->
+   </header><!-- End Header -->
+
+  <main id="main">
+
+    
+
+    
+
+    
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title"><br><br>
+          <h2>fill details for brochure</h2>
+          <p>write tagline</p>
+        </div>
+
+        
+             
+          <div class="col-lg-12">
+            <form action="forms/download.php" method="post">
+              <div class="form-row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                  <div class="validate"></div>
+                </div>
+                <div class="col-md-6 form-group">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                  <div class="validate"></div>
+                </div>
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                <div class="validate"></div>
+              </div>
+              <div class="form-group">
+                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                <div class="validate"></div>
+              </div>
+           <center>   <div><button class="btn-login" type="submit" name="submit">Send Message</button></div>
+           </center>
+            <!--  <div class="text-center"><button type="submit" name="submit">Send Message</button></div>-->
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+
+<!--footer-->
+<?php
+include 'footer.php';
+?>
+<script type="text/javascript">
+	
+  $(document).ready(function() {
+    $('body').bind('cut copy paste', function (e) {
+      e.preventDefault();
+    })
+    $("body").on("contextmenu", function(e) {
+      return false;
+    })
+  })
+  
+  </script>
+<!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="bx bx-up-arrow-alt"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
